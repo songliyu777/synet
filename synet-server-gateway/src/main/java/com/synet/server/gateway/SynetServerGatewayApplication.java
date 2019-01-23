f9e8980175d1636f8610c812fb10fef823a65349
Lliyu Song <39649728+songliyu777@users.noreply.github.com>
2019-01-23 22:32:38 +0800

    Revert "gateway"
    This reverts commit 1063acf35aed15fbc538797b3e01ea4a4c228e1d.

--- synet-server-gateway/src/main/java/com/synet/server/gateway/SynetServerGatewayApplication.java
+++ synet-server-gateway/src/main/java/com/synet/server/gateway/SynetServerGatewayApplication.java
@@ -2,11 +2,7 @@ package com.synet.server.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class SynetServerGatewayApplication {
 
@@ -14,9 +10,5 @@ public class SynetServerGatewayApplication {
 		SpringApplication.run(SynetServerGatewayApplication.class, args);
 	}
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public  String Test(){
-		return "Test1234";
-	}
 }
 
