e7cc30492a1fbcf33665a8382631fbb44d3473fa
songliyu <173621801@qq.com>
2019-01-23 12:49:34 +0800

    gateway

---
+++ synet-server-gateway/src/main/java/com/synet/server/gateway/SynetServerGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.synet.server.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynetServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynetServerGatewayApplication.class, args);
+	}
+
+}
+
