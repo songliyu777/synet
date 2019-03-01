eb8c2dc3d9346b6b31c60bfff390f26385d24103
songliyu <173621801@qq.com>
2019-03-01 16:11:37 +0800

    更新结构

--- synet-server-gateway/src/main/java/com/synet/server/gateway/SynetServerGatewayApplication.java
+++ synet-server-gateway/src/main/java/com/synet/server/gateway/SynetServerGatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.synet.protocol.*;
 
 @RestController
 @SpringBootApplication
