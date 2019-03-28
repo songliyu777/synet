0c84f4b83880a5ecfbaf19c8b61ec6f23bbb66df
songliyu <173621801@qq.com>
2019-03-28 14:51:10 +0800

    修改gitignore

--- synet-server-logic/src/main/java/com/synet/server/logic/config/RouteConfig.java
+++ synet-server-logic/src/main/java/com/synet/server/logic/config/RouteConfig.java
@@ -1,21 +1,21 @@
-package com.synet.server.logic.config;
-
-import com.synet.server.logic.controller.PostHandler;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
-
-@Configuration
-@EnableWebFlux
-public class RouteConfig {
-    @Bean
-    public RouterFunction<ServerResponse> routes(PostHandler postHandler){
-        return route(POST("/test").and(contentType(APPLICATION_OCTET_STREAM)), postHandler::test);
-    }
-}
+//package com.synet.server.logic.config;
+//
+//import com.synet.server.logic.controller.PostHandler;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM;
+//import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+//import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+//import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
+//
+//@Configuration
+//@EnableWebFlux
+//public class RouteConfig {
+//    @Bean
+//    public RouterFunction<ServerResponse> routes(PostHandler postHandler){
+//        return route(POST("/test").and(contentType(APPLICATION_OCTET_STREAM)), postHandler::test);
+//    }
+//}
